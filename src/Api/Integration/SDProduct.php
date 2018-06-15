4d67e160dab6d920622591e3c00a183e2b64aa75
Kane <52653784@qq.com>
2018-06-15 11:53:47 +0800

    测试

--- src/Api/Integration/SDProduct.php
+++ src/Api/Integration/SDProduct.php
@@ -3,7 +3,7 @@ namespace SD\Api\Integration;
 
 class SDProduct extends \SD\Api\MicroService {
 
-	const API_URL = '';
+	const API_URL = 'http://9-api/tester/';
 	const APP_TIME_ZONE = '';
 	const APP_TIME_FORMAT = '';
 
